7342076fed8ef15247e516182345a6da643ec2dc
XZNON <shivaliksingh09@gmail.com>
2025-11-05 16:03:44 +0530

    Issue page built

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/issues/new">Create Issue</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
